b04a7e664e5ea7255155ed75de241bb6ead2d7eb
sohib <sohibabed13@gmail.com>
2023-09-28 14:18:19 +0300

    dice game js

--- Dice.js
+++ Dice.js
@@ -5,7 +5,6 @@ const player1Title = document.getElementById("player1-title");
 const player2Title = document.getElementById("player2-title");
 const winnerMessage = document.getElementById("winner-message");
 
-// Rest of your JavaScript code...
 
 let currentPlayer = 1;
 let player1CurrentScore = 0;
@@ -13,17 +12,16 @@ let player2CurrentScore = 0;
 let player1TotalScore = 0;
 let player2TotalScore = 0;
 
-
 function diceGame() {
   //DICE 1
   var randomDice = Math.floor(Math.random() * 6 + 1);
-  var diceLocation = "images/" + "dice" + randomDice + ".png";
+  var diceLocation = "./images/" + "dice" + randomDice + ".png";
   var img1 = document.querySelectorAll("img")[0];
   img1.setAttribute("src", diceLocation);
 
   //DICE 2
   var randomDice2 = Math.floor(Math.random() * 6 + 1);
-  var diceLocation2 = "images/" + "dice" + randomDice2 + ".png";
+  var diceLocation2 = "./images/" + "dice" + randomDice2 + ".png";
   var img2 = document.querySelectorAll("img")[1];
   img2.setAttribute("src", diceLocation2);
 
@@ -34,7 +32,7 @@ function diceGame() {
     "Current Score: " + player1CurrentScore;
   document.getElementById("current-score-player2").textContent =
     "Current Score: " + player2CurrentScore;
-      checkWinner();
+  checkWinner();
 }
 
 function holdDice() {
@@ -42,74 +40,58 @@ function holdDice() {
   player2TotalScore += player2CurrentScore;
 
   document.getElementById("total-score-player1").innerHTML =
-  "total Score: " + player1TotalScore;
-document.getElementById("total-score-player2").innerHTML =
-  "total Score: " + player2TotalScore;
+    "total Score: " + player1TotalScore;
+  document.getElementById("total-score-player2").innerHTML =
+    "total Score: " + player2TotalScore;
 
   player1CurrentScore = 0;
-  player2CurrentScore = 0
+  player2CurrentScore = 0;
 
   document.getElementById("current-score-player1").innerHTML =
-  "Current Score: 0" 
-document.getElementById("current-score-player2").innerHTML =
-  "Current Score: 0" 
+    "Current Score: 0";
+  document.getElementById("current-score-player2").innerHTML =
+    "Current Score: 0";
   checkWinner();
   currentPlayer = currentPlayer === 1 ? 2 : 1;
   updatePlayerTitles();
 }
 function checkWinner() {
   if (player1TotalScore >= 30) {
-      declareWinner(1);
+    declareWinner(1);
   } else if (player2TotalScore >= 30) {
-      declareWinner(2);
+    declareWinner(2);
   }
 }
 
-
 function declareWinner(player) {
-  document.querySelector("h1").innerHTML= `Player ${player} Wins!`;
-  diceButton.disabled = true; // Disable the dice button when a player wins
-  holdButton.disabled = true; // Disable the "Hold Game" button when a player wins
+  document.querySelector("h1").innerHTML = `Player ${player} Wins!`;
+  diceButton.disabled = true;
+  holdButton.disabled = true;
 }
 function newGame() {
   player1CurrentScore = 0;
   player2CurrentScore = 0;
   player1TotalScore = 0;
   player2TotalScore = 0;
-  winnerMessage.textContent = '';
+  winnerMessage.textContent = "";
   currentPlayer = 1;
   updatePlayerTitles();
   diceButton.disabled = false;
   holdButton.disabled = false;
-  // Reset the displayed scores
-  document.getElementById('current-score-player1').textContent = "Current Score: 0";
-  document.getElementById('current-score-player2').textContent = "Current Score: 0";
-  document.getElementById('total-score-player1').textContent = "Total Score: 0";
-  document.getElementById('total-score-player2').textContent = "Total Score: 0";
+  document.getElementById("current-score-player1").textContent =
+    "Current Score: 0";
+  document.getElementById("current-score-player2").textContent =
+    "Current Score: 0";
+  document.getElementById("total-score-player1").textContent = "Total Score: 0";
+  document.getElementById("total-score-player2").textContent = "Total Score: 0";
 }
 
 function updatePlayerTitles() {
   if (currentPlayer === 1) {
-      player1Title.textContent = "Player 1 (Your Turn)";
-      player2Title.textContent = "Player 2";
+    player1Title.textContent = "Player 1 (Your Turn)";
+    player2Title.textContent = "Player 2";
   } else {
-      player1Title.textContent = "Player 1";
-      player2Title.textContent = "Player 2 (Your Turn)";
+    player1Title.textContent = "Player 1";
+    player2Title.textContent = "Player 2 (Your Turn)";
   }
 }
-
-
-// function checkWinner() {
-//   if (player1TotalScore >= 30) {
-//     document.querySelector("h1").innerHTML  = "Player 1 (WINNER) &#128681";
-//     dice.disabled = true;
-//     hold.disabled = true; 
-//   }
-
-//   if (player2TotalScore >= 30) {
-//     document.querySelector("h1").innerHTML  = " &#128681 Player 2 (WINNER)";
-//     dice.disabled = true; 
-//     holdButton.disabled = true; 
-//   }
-// }
-
